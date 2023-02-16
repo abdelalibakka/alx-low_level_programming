14aaa50abbc85d264d7dc0ea290a4d6a0552286d
abdelalibakka <pakkatatata@gmail.com>
2023-02-16 15:20:13 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
-
 /**
-* main - A program that prints a line with puts function
-* Return: 0 (success)
-*/
+ * main - main block
+ * Return: 0
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
-Task 5
-#include <stdio.h>
-/**
-* main - A program that print a line using the printf function
-* Return: 0 (success)
-*/
+
