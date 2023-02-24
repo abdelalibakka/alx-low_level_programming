ecd13d16c987a00f6854456373bd0319b5fa79a3
abdelalibakka <pakkatatata@gmail.com>
2023-02-23 18:37:15 -0800

    task&101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,27 +1,26 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * print_number - prints an integer
- * @n: integer to be printed
+ * main - prime numbers
+ *
+ * Return: 0
  */
-void print_number(int n)
+int main(void)
 {
-	unsigned int n1;
+	long num = 612852475143;
+	long divisor = 2;
+	long larg_prim = 0;
 
-	if (n < 0)
+	while (num != 1)
 	{
-		n1 = -n;
-		_putchar('-');
-	} else
-	{
-		n1 = n;
+		if (num % divisor == 0)
+		{
+			num = num / divisor;
+			larg_prim = divisor;
+		}
+		divisor += 1;
+
 	}
-
-	if (n1 / 10)
-	{
-		print_number(n1 / 10);
-	}
-
-	_putchar((n1 % 10) + '0');
+	printf("%ld\n", larg_prim);
+	return (0);
 }
-
