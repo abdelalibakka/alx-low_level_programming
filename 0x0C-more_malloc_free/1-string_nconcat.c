a7ed1680090cccbae7b43064f48f2fa8573fac0e
abdelalibakka <pakkatatata@gmail.com>
2023-03-16 18:22:52 -0700

    tasks

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,90 +1,47 @@
-#include <stdlib.h>
-
 #include "main.h"
-
-
-
+#include <stdlib.h>
 /**
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
+ * Return: integer length of string
+ */
 
- * *string_nconcat - concatenates n bytes of a string to another string
+int _strlen(char *s)
+{
+	int i = 0;
 
- * @s1: string to append to
-
- * @s2: string to concatenate from
-
- * @n: number of bytes from s2 to concatenate to s1
-
- *
-
- * Return: pointer to the resulting string
-
+	while (*s++)
+		i++;
+	return (i);
+}
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: copy first n bytes of s2
+ * Return: void
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
-
 {
+	char *s, *r;
+	unsigned int l1, l2;
 
-	char *s;
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
+	l1 = _strlen(s1);
+	l2 = _strlen(s2);
 
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
-
-
-
-	while (s1 && s1[len1])
-
-		len1++;
-
-	while (s2 && s2[len2])
-
-		len2++;
-
-
-
-	if (n < len2)
-
-		s = malloc(sizeof(char) * (len1 + n + 1));
-
-	else
-
-		s = malloc(sizeof(char) * (len1 + len2 + 1));
-
-
-		
+	l2 = l2 > n ? n : l2;
+	r = s = malloc(l1 + l2 + 1);
 	if (!s)
-
 		return (NULL);
-
-
-
-	while (i < len1)
-
-	{
-
-		s[i] = s1[i];
-
-		i++;
-
-	}
-
-
-		
-	while (n < len2 && i < (len1 + n))
-
-		s[i++] = s2[j++];
-
-
-		
-	while (n >= len2 && i < (len1 + len2))
-
-		s[i++] = s2[j++];
-
-
-
-	s[i] = '\0';
-
-
-
-	return (s);
-
+	while (*s1)
+		*s++ = *s1++;
+	while (l2--)
+		*s++ = *s2++;
+	*s = '\0';
+	return (r);
 }
-
--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,60 +1,25 @@
-#include <stdlib.h>
-		
 #include "main.h"
-		
-
-		
+#include <stdlib.h>
+#include <stdio.h>
 /**
-		
- * *array_range - creates an array of integers
-		
- * @min: minimum range of values stored
-		
- * @max: maximum range of values stored and number of elements
-		
- *
-		
- * Return: pointer to the new array
-		
+ * array_range - creates array of integers
+ * @min: the smallest integer
+ * @max: the largest integer
+ * Return: int pointer to array
  */
-		
+
 int *array_range(int min, int max)
-		
 {
-		
-	int *ptr;
-		
-	int i, size;
-		
+	int *p, n;
 
-		
 	if (min > max)
-		
 		return (NULL);
-		
-
-		
-	size = max - min + 1;
-		
-
-		
-	ptr = malloc(sizeof(int) * size);
-		
-
-		
-	if (ptr == NULL)
-		
+	n = max - min + 1;
+	p = malloc(sizeof(*p) * n);
+	if (!p)
 		return (NULL);
-		
-
-		
-	for (i = 0; min <= max; i++)
-		
-		ptr[i] = min++;
-		
-
-		
-	return (ptr);
-		
+	while (n--)
+		p[n] = max--;
+	return (p);
 }
 
