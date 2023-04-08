43662a49a150c744ebfc889d4207b59487398b35
abdelalibakka <pakkatatata@gmail.com>
2023-04-07 20:14:14 -0700

    100-get_endianness.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,25 +1,52 @@
 #include "main.h"
+#include <stddef.h>
 
+int _pow(int x, int y);
 /**
- * binary_to_uint - converts a binary number to unsigned int
- * @b: string containing the binary number
- *
- * Return: the converted number
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: pointer to a string of 0 and 1 chars
+ * Return: converted number
+ *  or 0 if b is NULL or strin B contains char other than 0 and 1
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int dec_val = 0;
+	int i, n = 0;
 
-	if (!b)
+	unsigned int res = 0;
+
+	if (b == NULL)
 		return (0);
 
 	for (i = 0; b[i]; i++)
 	{
-		if (b[i] < '0' || b[i] > '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
-	}
 
-	return (dec_val);
+		n++;
+	}
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] == '1')
+		{
+			res += _pow(2, n - 1);
+		}
+		n--;
+	}
+	return (res);
+}
+/**
+ * _pow - returns the value of x raised to power of y
+ * @x: base number
+ * @y: power of
+ * Return: x to the power of y
+ */
+int _pow(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+
+	return (x * _pow(x, y - 1));
 }
