2b67b9c5e5617db9a6904f1ed16e912c9c649601
abdelalibakka <pakkatatata@gmail.com>
2023-04-08 17:49:36 -0700

    tasks

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,52 +1,25 @@
 #include "main.h"
-#include <stddef.h>
 
-int _pow(int x, int y);
 /**
- * binary_to_uint - converts a binary number to an unsigned int
- * @b: pointer to a string of 0 and 1 chars
- * Return: converted number
- *  or 0 if b is NULL or strin B contains char other than 0 and 1
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
+ *
+ * Return: the converted number
  */
-
 unsigned int binary_to_uint(const char *b)
 {
-	int i, n = 0;
+	int i;
+	unsigned int dec_val = 0;
 
-	unsigned int res = 0;
-
-	if (b == NULL)
+	if (!b)
 		return (0);
 
 	for (i = 0; b[i]; i++)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-
-		n++;
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-	for (i = 0; b[i]; i++)
-	{
-		if (b[i] == '1')
-		{
-			res += _pow(2, n - 1);
-		}
-		n--;
-	}
-	return (res);
-}
-/**
- * _pow - returns the value of x raised to power of y
- * @x: base number
- * @y: power of
- * Return: x to the power of y
- */
-int _pow(int x, int y)
-{
-	if (y < 0)
-		return (-1);
-	if (y == 0)
-		return (1);
 
-	return (x * _pow(x, y - 1));
+	return (dec_val);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,24 +1,25 @@
-/**
- * flip_bits - counts the number of bits to change
- * to get from one number to another
- * @n: first number
- * @m: second number
- *
- * Return: number of bits to change
- */
-unsigned int flip_bits(unsigned long int n, unsigned long int m)
-{
-	int i, count = 0;
-	unsigned long int current;
-	unsigned long int exclusive = n ^ m;
+#ifndef MAIN_H
 
-	for (i = 63; i >= 0; i--)
-	{
-		current = exclusive >> i;
-		if (current & 1)
-			count++;
-	}
+#define MAIN_H
 
-	return (count);
-}
 
+unsigned int binary_to_uint(const char *b);
+
+void print_binary(unsigned long int n);
+
+int get_bit(unsigned long int n, unsigned int index);
+
+int set_bit(unsigned long int *n, unsigned int index);
+
+int clear_bit(unsigned long int *n, unsigned int index);
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+
+int _atoi(const char *s);
+
+int _putchar(char c);
+
+int get_endianness(void);
+
+
+#endif
