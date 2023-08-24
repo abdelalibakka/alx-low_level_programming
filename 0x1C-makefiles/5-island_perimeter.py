1ae8e56c45aeaf38578e56cb2f85fc39d952a8ac
abdelalibakka <pakkatatata@gmail.com>
2023-08-23 17:48:07 -0700

    update

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""explainalation oof an_island perimeter_measuring funct"""
+
+
+def island_perimeter(grid):
+    """Return_perimeter_of an_island.
+    """
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
