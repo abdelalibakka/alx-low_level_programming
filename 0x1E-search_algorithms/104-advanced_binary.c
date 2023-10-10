9af7ccb12c2b942372b7a6f3bfdd1eee50dc42e0
abdelalibakka <pakkatatata@gmail.com>
2023-10-10 16:34:23 -0700

    104-advanced_binary.c

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,47 +1,45 @@
+#include <stdio.h>
 #include "search_algos.h"
 
-
 /**
- * advanced_binary_recursive - performs_recursive_binary_search
- * @array: pointer_to_the_first_element
- * @right: starting_index
- * @left: ending_index
- * @value: value_to_search_for
- * Return: -1 || index_where_value_is_located
+ * advanced_binary - Searches for a value in a sorted array of integers.
+ * @array: Pointer to the first element of the array to search in.
+ * @size: Number of elements in the array.
+ * @value: The value to search for.
+ * Return: The index where the value is located or -1 if not found.
  */
-
-int advanced_binary_recursive(int *array, size_t left, size_t right, int value)
-{
-	size_t i;
-
-	if (right < left)
-		return (-1);
-
-	printf("Searching in array: ");
-	for (i = left; i < right; i++)
-		printf("%d, ", array[i]);
-	printf("%d\n", array[i]);
-
-	i = left + (right - left) / 2;
-	if (array[i] == value && (i == left || array[i - 1] != value))
-		return (i);
-	if (array[i] >= value)
-		return (advanced_binary_recursive(array, left, i, value));
-	return (advanced_binary_recursive(array, i + 1, right, value));
-}
-
-/**
- * advanced_binary - searches_a_value in_a sorted_array_of_intergers
- * @array: pointer_to_the first_element of_the_array
- * @size: number_of_elements_in_array
- * @value: value_to_search_for
- * Return: value
- */
-
 int advanced_binary(int *array, size_t size, int value)
 {
-	if (array == NULL || size == 0)
-		return (-1);
+    size_t i;
+    int middle;
 
-	return (advanced_binary_recursive(array, 0, size - 1, value));
+    if (array == NULL)
+        return (-1);
+
+    printf("Searching in array: ");
+    for (i = 0; i < size; i++)
+    {
+        printf("%d", array[i]);
+        if (i < size - 1)
+            printf(", ");
+    }
+    printf("\n");
+
+    if (size == 1 && array[0] != value)
+        return (-1);
+
+    middle = (size - 1) / 2;
+
+    if (array[middle] == value)
+    {
+        if (middle > 0 && array[middle - 1] == value)
+            return advanced_binary(array, middle + 1, value);
+        return (middle);
+    }
+
+    if (array[middle] < value)
+    {
+        return advanced_binary(array + middle + 1, size - middle - 1, value) + middle + 1;
+    }
+    return advanced_binary(array, middle, value);
 }
